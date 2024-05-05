bf4c2951839ebdde24a700d871907c60c0ac55ff
Paolo Ardolino <paolohardolino@gmail.com>
2024-05-05 16:34:52 +0200

    First Commit

---
+++ src/test/java/com/paolo/drumkit/DrumKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paolo.drumkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrumKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
