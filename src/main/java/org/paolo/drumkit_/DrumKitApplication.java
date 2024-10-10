d9f8e7f7f27e90ab84681a03ea5192d093bab462
paolo <paolohardolino@gmail.com>
2024-10-10 08:37:10 +0200

    model

---
+++ src/main/java/org/paolo/drumkit_/DrumKitApplication.java
@@ -0,0 +1,13 @@
+package org.paolo.drumkit_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrumKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrumKitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/paolo/drumkit_/DrumKitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.paolo.drumkit_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrumKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
